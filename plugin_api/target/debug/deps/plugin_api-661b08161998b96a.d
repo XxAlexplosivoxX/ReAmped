72f0639050c850034c102b2aae373055d50caf5f
XxAlex_plosivoxX <Alex_plosivo_777@proton.me>
2026-03-12 20:22:19 -0600

    fix target indexed on plugin api

--- plugin_api/target/debug/deps/plugin_api-661b08161998b96a.d
+++
@@ -1,5 +0,0 @@
-/home/wilxd/learn-rust/ReAmped/plugin_api/target/debug/deps/plugin_api-661b08161998b96a.d: src/lib.rs
-
-/home/wilxd/learn-rust/ReAmped/plugin_api/target/debug/deps/libplugin_api-661b08161998b96a.rmeta: src/lib.rs
-
-src/lib.rs:
--- plugin_api/target/debug/deps/plugin_api-e366ec172fbb54ea.d
+++
@@ -1,5 +0,0 @@
-/home/wilxd/learn-rust/ReAmped/plugin_api/target/debug/deps/plugin_api-e366ec172fbb54ea.d: src/lib.rs
-
-/home/wilxd/learn-rust/ReAmped/plugin_api/target/debug/deps/libplugin_api-e366ec172fbb54ea.rmeta: src/lib.rs
-
-src/lib.rs:
